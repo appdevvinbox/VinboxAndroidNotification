6f6183f33f5639edea30c7e1a588d4af86855d95
ravi-vinmax <ravi@vinbox.in>
2022-09-21 00:05:10 +0000

    intial cbi release - 1.0.5

--- vinmax/src/main/java/com/vinbox/vinmax/MessagingService.java
+++ vinmax/src/main/java/com/vinbox/vinmax/MessagingService.java
@@ -36,7 +36,6 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onNewToken(@NonNull String token) {
-        Log.d(TAG, "Refreshed token: " + token);
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
